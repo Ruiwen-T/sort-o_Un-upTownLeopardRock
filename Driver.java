4314d69633d0ade95441adf144b55c94dbcafb3b
Lea Kwok <lea.a.kwok@gmail.com>
2022-01-10 13:18:41 -0500

    updating file

--- Driver.java
+++ Driver.java
@@ -2,9 +2,21 @@ import java.util.ArrayList;
 
 public class Driver{
 	public static void main(String[] args){
-		ArrayList<Comparable> test1 = Sorts.populate(50, 0, 50 );
+		ArrayList<Comparable> test1 = new ArrayList<Comparable>();
+		
+		for(int i = 0; i < 10; i++){
+			test1.add(i);
+		}
 		
 		System.out.println("Testing bubbleSort:");
+		System.out.println("Array before sorting: " );
+		System.out.println(test1);
+			
+		Sorts.bubbleSortV(test1);
+			
+		System.out.println("Array after sorting: ");
+		System.out.println(test1);
+		
 		
 		for(int i = 0; i < 20; i++){
 			System.out.println("Array before sorting: " );
